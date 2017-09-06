5754b79065b5a2ee8f3f8dc15fa0fd2e7070989c
mark0v <amark0vv@gmail.com>
2017-09-07 01:10:29 +0300

    Fix commit error.

---
+++ homeWorks/src/ClosestNum.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+/**
+ * Created by onedell on 07.09.2017.
+ */
+public class ClosestNum {
+    int firstEntered;
+    int secondEntered;
+    int etalonNum;
+
+    public int findDelta(int Num, int Etalon){
+        if (Num <= Etalon){
+            return Etalon - Num;
+        }else{
+            return Num - Etalon;
+        }
+    }
+
+    public void setUpNumbers(){
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter First number: ");
+        firstEntered = in.nextInt();
+        System.out.println("Your input is: " + firstEntered);
+
+        System.out.println("Enter Second number: ");
+        secondEntered = in.nextInt();
+        System.out.println("Your input is: " + secondEntered);
+
+        System.out.println("Enter Etalon number: ");
+        etalonNum = in.nextInt();
+        System.out.println("Your input is: " + etalonNum);
+    }
+
+
+    public void findClosestNum(int firstDelta, int secondDelta){
+        if(firstDelta == secondDelta){
+            System.out.println("Entered Numbers is equals!");
+        }else if(firstDelta < secondDelta){
+            System.out.println("Number closer to etalon: " + firstEntered);
+        }else{
+            System.out.println("Number closer to etalon: " + secondEntered);
+        }
+    }
+}
--- homeWorks/src/Starter.java
+++ homeWorks/src/Starter.java
@@ -2,7 +2,7 @@ public class Starter {
 
     public static void main(String args[ ]){
 
-        ClosestNumber c = new ClosestNumber();
+        ClosestNum c = new ClosestNum();
         c.setUpNumbers();
         int firstDelta = c.findDelta(c.firstEntered, c.etalonNum);
         int secondDelta = c.findDelta(c.secondEntered, c.etalonNum);
